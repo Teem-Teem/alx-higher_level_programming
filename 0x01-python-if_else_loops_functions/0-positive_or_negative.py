2cddc4d8b536250bde8dbb0821a136f687b77578
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2022-11-29 22:52:37 +0000

    +ve or -ve"

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random. randiant(-10, 10)
+if number > 0:
+    print(f"{number:d} is positive")
+elif number == 0:
+    print(f"{number:d} is zero")
+else:
+    print(f"{number:d} is negative")
