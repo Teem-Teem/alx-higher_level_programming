b51d692992055425ff50bcd2ab03d65401972182
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2022-11-30 21:29:50 +0000

    combo3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i is not 8:
-            print("{}{}".format(i, j), end=", ")
-        else:
-            print("{}{}".format(i, j))
+for i in range(0, 9):
+    for j in range(1, 10):
+        if j > i:
+            print("{:d}{:d}".format(i, j), end="")
+            if i != 8:
+                print(", ", end="")
+            else:
+                print("")
