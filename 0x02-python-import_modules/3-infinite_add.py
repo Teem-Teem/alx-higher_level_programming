2c751d1f4e36d327f1e48e8739870bbb8e8762e7
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2022-12-02 02:24:00 +0000

    3-infinte

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
-import sys
-
-
 if __name__ == "__main__":
-        temp = 0
-        for i in range(1, len(sys.argv)):
-            temp += int(sys.argv[i])
-        print(temp)
+    import sys
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    res = 0
+    while index <= argv_count:
+        res += int(sys.argv[index])
+        index += 1
+    print("{:d}".format(res))
