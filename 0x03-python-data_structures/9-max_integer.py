e40cb819e8e61a4b161eede9390a86b62d434037
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2022-12-06 18:48:51 +0000

    Max_integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
+def max_integer(my_list):
     if not my_list:
-        return None
-    biggest = 0
-    for index in my_list:
-        if index > biggest:
-            biggest = index
-    return biggest
+        return (None)
+    large = my_list[0]
+    for i in my_list:
+        if i > large:
+            large = i
+    return (large)
