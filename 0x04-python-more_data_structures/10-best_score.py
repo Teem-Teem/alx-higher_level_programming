b307892c3ad6d1e48774a1c1741ae3bf4a337082
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2022-12-08 02:10:28 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,4 @@ def best_score(my_dict):
     if not my_dict:
         return None
     return "{:s}".format([key for key, v in my_dict.items()
-                        if v == max(my_dict[x] for x in my_dict)][0])
+                          if v == max(my_dict[x] for x in my_dict)][0])
