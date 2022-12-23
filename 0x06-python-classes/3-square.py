9a7af5cd87757e3c57358052a8d10f938efaa40c
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2022-12-23 05:26:19 +0000

    Squared improved

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 0-quare.py by Ehoneah Obed
+# 0-square.py by Ehoneah Obed
 """A module that defines a square """
 
 
@@ -20,4 +20,12 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
 
-        self.__size = 
+                                                                                                self.__size = size
+
+                                                                                            def area(self):
+                                                                                                """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
+        return (self.__size ** 2)
