44fb8c78be7009806e99367ddb9975b358309621
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2023-01-09 14:30:32 +0000

    the test file eh

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""Inherits from list"""
-
-
 class MyList(list):
-        """inherits from list"""
-
-        def print_sorted(self):
-            """print sorted list"""
-            print(sorted(self))
+    def print_sorted(self):
+        print(sorted(self))
