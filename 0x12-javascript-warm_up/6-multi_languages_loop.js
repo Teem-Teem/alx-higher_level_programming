063e9def456de765413bd26c37665d6dc87cc2e2
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2023-03-13 23:43:14 +0000

    i love c in java

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0, len = myArray.length; i < len; i++) {
-  console.log(myArray[i]);
-}
+// ['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach((e) => console.log(e));
+//
+// const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+//
+// msg.forEach((element) => console.log(element));
