0abd1995ed5cc3c8a5e3d11e0161c579f02e1ae1
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2023-08-15 02:22:41 +0000

    second biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-
-let argv = process.argv;
-
-if (argv.length < 4) { console.log(0); } else {
-
-  argv = argv.slice(2);
-
-  let nums = argv.map(x => parseInt(x));
-  nums = nums.sort(function (a, b) { return a - b; });
-
-  console.log(nums[nums.length - 2]);
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+   console.log(args[args.length - 2]);
 }
