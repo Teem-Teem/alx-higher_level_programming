857c24a595153ee36dc0e7b5edb18cc37857c5f7
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2023-08-15 00:43:05 +0000

    Java arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-
-// a script that prints boolean of each time the arguments found
-
-if (process.argv.length < 3) {
-	  console.log('No argument');
+if (process.argv.length === 2) {
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-	  console.log('Argument found');
+  console.log('Argument found');
 } else {
-	  console.log('Arguments found');
+  console.log('Arguments found');
 }
