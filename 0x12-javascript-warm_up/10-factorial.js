50a6853ee18081edf61258adb2514fa139d4f9fe
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2023-08-15 01:47:56 +0000

    computing and printing a fctorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const x = parseInt(process.argv[2]);
+function findFactorial (x) {
+  if (!x) { return 1; }
+
+  if (x <= 0) { return; }
+  return findFactorial(x - 1) * x;
+}
+
+console.log(findfactorial(x));
