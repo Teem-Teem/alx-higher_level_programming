396792ed41407d6804064a81e979b9c7af6db547
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2023-08-19 23:46:43 +0000

    script that lists all states from btn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""
-List all state in the database 'hbtn_0e_0_usa'.
-"""
+""" List all state in database """
 
-import MySQLdb
+
 from sys import argv
+import MySQLdb
 
 if __name__ == '__main__':
 
-    db_username = argv[1]
-    db_password = argv[2]
+    db_user = argv[1]
+    db_passwd = argv[2]
     db_name = argv[3]
 
-    db = MySQLdb.connect(host="localhost", username=arg[1], port=3306,
-                         password=argv[2], db=argv[3])
+    database = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=db_user,
+                               passwd=db_passwd,
+                               db=db_name)
 
+    cursor = database.cursor()
 
-    cursor = databse.cursor()
+    cursor.execute('SELECT id, name FROM states ORDER BY states.id ASC')
 
-    cursor.execute("SELECT * FROM states")
-    row = cursor.fetchall()
-
-    for row in rows:
+    for row in cursor.fetchall():
         print(row)
 
