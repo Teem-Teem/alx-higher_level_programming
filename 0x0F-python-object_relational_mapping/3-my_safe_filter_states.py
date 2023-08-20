e7824c5d4e5ff252946d19a3e5bcfa62259805db
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2023-08-20 23:16:48 +0000

    script that is safe from MySQL injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,13 +15,19 @@ if __name__ == '__main__':
     search = '{}'.format(argv[4])
 
     db = MySQLdb.connect(host="localhost", port=3306, user=db_user,
-                         passwd=db_passwd, db=db_name)
+                         passwd=db_passwd, db=argv[3])
 
     cur = db.cursor()
 
     cur.execute("SELECT id, name FROM states\
                 WHERE name = %s\
                 ORDER BY states.id ASC;", (search,))
+    {
+    "name" : argv[4]
+    })
 
-    for row in cur.fetchall():
+
+    rows = cur.fetchall()
+
+    for row in rows:
         print(row)
