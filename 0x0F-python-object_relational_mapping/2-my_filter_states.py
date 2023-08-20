d5c3e77f10dfe2ee190a77263b0076dde65677c9
Teem-Teem <olaniyioluwatimilehin@gmail.com>
2023-08-20 00:12:59 +0000

    displays all values in the states table of hbtn_usa whr name matches d arg

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE"
                    " '{:s}' ORDER BY id ASC".format(argv[4]))
-    db = cursor.fetchall()
-    for i in db:
-        if i[1] == argv[4]:
-            print(i)
+    rows = cursor.fetchall()
+    
+    for row in rows:
+        print(row)
